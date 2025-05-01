e2e9b5f2f5b6d4d118bbb7f4aea684ee65eda253
lavc4645 <lavc4645@gmail.com>
2025-05-01 16:54:07 +0530

    testing(api): add integration tests for user routes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.conrtoller";
 import { AuthService } from "./auth.service";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
+    imports:[PrismaModule],
     controllers:[AuthController],
     providers:[AuthService],
 })
