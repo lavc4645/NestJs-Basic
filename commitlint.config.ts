3f9e6e44919f4dba1dabba16e8632677e3a532d0
lavc4645 <lavc4645@gmail.com>
2025-05-02 14:47:40 +0530

    chore(release): version 0.0.2

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,20 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'refactor',
+        'test',
+        'testing', // ✅ added this
+        'chore',
+        'ci',
+        'build',
+      ],
+    ],
+  },
 };
